41edeb4d38c6ceb9c75b63d811fa798871a151d9
Alexander Leong <toshibaalexander@gmail.com>
2022-03-03 07:18:52 +1000

    cleanup code, add doc build step

--- src/util.jl
+++ src/util.jl
@@ -19,17 +19,3 @@ function kspace_to_img(img_path="")
   img_new_new[Int64((end/2)+1):end, 1:end] = img_new[1:Int64(end/2), 1:end];
   return img_new_new;
 end
-
-function plot_mri(img)
-  img_min, img_max = findmin(img)[1], findmax(img)[1]
-
-  # normalize values between 0 and 1
-  img = (img .- img_min) ./ (img_max - img_min)
-  img = [vec(img[:,i]) for i in 1:size(img)[2]]
-  return plot(heatmap(z=img, colorscale="Greys"), Layout(font_color="white", paper_bgcolor="#222222"))
-end
-
-function plot_raw_mri(img)
-  img = plot_mri(img)
-  return img
-end
